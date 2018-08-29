b94a0fc2f1a3408f80980a28465bdde37f23415c
Akil <achsvgusaeng@yahoo.com>
2018-08-29 20:57:02 +0000

    new file for async/await

---
+++ js/newaa.js
@@ -0,0 +1,12 @@
+const fetch = require("node-fetch");
+
+async function fetchAvaterUrl(userId){
+    
+    
+    // return fetch(`https://catappapi.herokuapp.com/users/${userId}`) 
+    // .then(response => response.json())
+    // .then(data => data.imageUrl)
+}
+
+const result = fetchAvaterUrl(123) 
+result 
